118112c28e91927143175e0c3dc6efca45656f21
Robert Lafferty <frogboythatsme@gmail.com>
2025-10-26 20:37:54 -0700

    Fixing broken image paths

--- src/components/CharacterCard.jsx
+++ src/components/CharacterCard.jsx
@@ -4,7 +4,7 @@ export default function CharacterCard({ character }) {
   return (
     <div className={`card text-center`}>
       <img
-        src={character.portrait}
+        src={import.meta.env.BASE_URL + character.portrait}
         alt={character.name}
         // className="w-32 h-32 object-cover rounded-full mx-auto mb-3 shadow-parchment"
       />
