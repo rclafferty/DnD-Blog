fb02d8a635056cf81eedb26cc5688f062080eb6e
rclafferty <rclafferty@users.noreply.github.com>
2026-01-18 21:28:20 +0000

    deploy: 3e3fc9dad420137e6f9c5393074a7556e0869044

---
+++ assets/Journal-A-Party-Is-Formed-pxCLAX_9.js
@@ -0,0 +1,311 @@
+var e=`# A Party is Formed!
+
+:::introduction
+The dungeon master starts the session with introduction scenes to introduce each individual character, establish personality, and build rapore
+:::
+
+:::stage-notes
+Enter Caesar and his character, Zaelrin.
+:::
+
+Zaelrin is in a town center with his gear. By trade, Zaelrin is a contortionist and a poet, where he will contort his body into the shape of an inanimate object and recite a poem about that object. Zaelrin is mid-performance at the start of this scene.
+
+:::stage-notes
+Zaelrin's player recites a prepared poem
+:::
+
+:stat-roll[Zaelrin rolls a 1]
+
+:player-aside[We as a party have never seen such an epic flop at the start of any game or campaign, and likely will never see one again! This was great!]
+
+---
+
+:::stage-notes
+Cut to Casey and his character, Thoradin.
+:::
+
+Thoradin is in the Great War as a sargean.
+
+:stat-roll[Thoradin rolls a 10 + 3 Perception]
+
+He encounters a young recruit who is frightened and has wet himself. "I don't know if I can do this anymore."
+
+:stat-roll[Thoradin rolls a 12 - 1 Persuasion]
+
+Thoradin attempts to encourage the recruit but has a gruff personality. "Get yourself together and get back out there so we can all get home!"
+
+The recruit seems to be encouraged and willing to return to the fight.
+
+---
+
+:::stage-notes
+Cut to Zaelrin
+:::
+
+Zaelrin is sneaking through an alley. Zaelrin has been hired to plant a package in a house in this alley and leave quietly.
+
+:stat-roll[Zaelrin rolls a 5 + 4 + 7 Stealth]
+
+Zaelrin peeks into a window of the house, but doesn't see anything.
+
+:stat-roll[Zaelrin rolls a 4 + 3 Perception]
+
+Zaelrin attempts to open the window to enter the house
+
+:stat-roll[Zaelrin rolls a 11 + 4 + 7 Stealth]
+
+Zaelrin successfully opens the window and climbs through, leaves the package nearby, and proceeds to leave. On the way out, Zaelrin hears a nearby child and begins to wonder what the package may be, only now suddenly concerned about what he has delivered. Was it dangerous? Was it poison? We shall never know.
+
+---
+
+:::stage-notes
+Cut to Thoradin
+:::
+
+Thoradin is back in the Great War hiking along a path. There is a boulder in his path, blocking him from reaching a trapped companion, Woods, on the other side.
+
+Thoradin attempts to leap over the boulder.
+
+:stat-roll[Thoradin rolls a 18 + 5 Athletics]
+
+Thoradin successfully jumps over the boulder blocking his path and finds his companion trapped under a slightly smaller boulder that has been launched from a catapult.
+
+Thoradin attempts to pick up the boulder and throw it back at the enemy.
+
+:stat-roll[Thoradin rolls a 6 + 5 Athletics]
+
+Thoradin successfully frees his companion by picking up the boulder, but is unsuccessful in launching it back at the enemy. Woods is freed!
+
+:player-aside[DM: "You're not the Hulk, but that was a good try!"]
+
+---
+
+:::stage-notes
+Cut to Juan and his character, Michizane
+:::
+
+Michizane is speaking with a concerned citizen about the town being raided recently. Michizane has led efforts to fend off these raids before but the attacks are getting worse.
+
+Michizane assures the citizen: "Our allies are guarding the town," which appears to calm the citizen.
+
+---
+
+:::stage-notes
+Cut to a field where all three party members are present
+:::
+
+Thoradin sees Zaelrin, who asks "Where might you be heading?" Thoradin responds with "I'm going where the wind takes me!"
+
+Michizane is curious about the encounter but chooses to climb a tree and observe from afar.
+
+:stat-roll[Thoradin rolls a 13 + 3 perception]
+
+:stat-roll[Zaelrin rolls a ?? perception]
+
+Thoradin notices Michizane in a tree. Having just returned from the war, Thoradin suspects Michizane is a spy and beggins chopping the tree down in an effort to confront Michizane.
+
+Michizane frantically yells: "I'm on your side!"
+
+:player-aside[Thoradin sees man in tree. First reaction: Chop tree down! Not quite the reaction you'd expect, but it was hilarious!]
+
+:stat-roll[Thoradin rolls a 4 + 3 Perception]
+
+Caesar hears an enemy party approaching, but Thoradin is too wrapped up in his confrontation with Michizane to notice the enemy. An arrow shoots by, misses both Zaelrin and Thoradin, hitting the tree holding Michizane.
+
+:stat-roll[Thoradin rolls a 13 + 1 Initiative]
+
+:::stage-notes
+Combat begins! The order of the combat goes as follows:
+- Michizane
+- Bandits (5)
+- Thoradin
+- Zaelrin
+:::
+
+Michizane does a front flip from the tree, arranges a temporary truce with Thoradin, and proceeds to shoot a bow at one of the bandits. Michizane hits the bandit in the leg :stat-roll[(5 damage)], causing the bandit to limp.
+
+:player-aside["I was an adventurer like you, until I took an arrow to the knee!"]
+
+The five bandits are in short bow range.
+
+:::stage-notes
+Some mechanics notes:
+- D20 Attack roll is to determine if the attack hits/misses
+- D6 Damage roll is to determine how much damage is done
+- The attack roll must be higher than the enemy's AC
+:::
+
+The bandits shoot arrows at Thoradin, Zaelrin (x3), and Michizane, missing all five shots.
+
+Thoradin switches to a light crossbow and proceeds to target the injured bandit.
+
+:stat-roll[Thoradin rolls a 3 + 1 + 2 Attack]
+
+Thoradin misses the bandit.
+
+Zaelrin uses a cantrip to hide and wait for the bandits to approach.
+
+:stat-roll[Zaelrin rolls a 4 + 1 + 7 Stealth]
+
+Michizane casts Entangle on the bandits, releasing vines in a wide area in a show of fancy green lights.
+
+:stat-roll[Michizane rolls to determine the number of bandits affected: 4]
+
+:::stage-notes
+Bandits roll strength saving throws to protect against the Entangle spell.
+:::
+
+:stat-roll[Bandit #1 rolls a 13 and is therefore unaffected by Entangle]
+:stat-roll[Bandit #2 rolls a 7 and is therefore caught in Entangle]
+:stat-roll[Bandit #3 rolls a 4 and is therefore caught in Entangle]
+:stat-roll[Bandit #4 rolls a 15 and is therefore unaffected by Entangle]
+
+:::stage-notes
+Bandit #5 and Michizane's allies are unaffected by Entangle. For the two bandits caught in Entangle, there is now difficult terrain that reduces the movement speed by half the normal speed.
+:::
+
+The five bandits proceed to approach the party, with the three unaffected by Entangle now in melee range.
+
+The bandits attack Michizane first
+
+:stat-roll[Bandit rolls a 15 + 3 Attack]
+
+The bandits have injured Michizane
+
+Two of the bandits then attack Thoradin.
+
+:stat-roll[Bandit rolls a 15]
+
+:stat-roll[Bandit rolls a 14]
+
+Thoradin narrowly avoids both attacks.
+
+The bandits then attack Michizane again.
+
+:stat-roll[Bandit rolls a 13]
+
+Michizane easily dodges the attack.
+
+Thoradin switches to his two-handed axe and charges at Bandit #1
+
+:stat-roll[Thoradin rolls a 1]
+
+Thoradin is distracted by the nearby vines, missing the bandit and instead hitting the vines.
+
+Zaelrin creates an illusion of crowd noises only audible to the bandits. Bandit #5 runs off scared.
+
+Michizane casts Shilaela at Bandit #1.
+
+:stat-roll[Michizane rolls a 19 Attack]
+
+Michizane hits the bandit.
+
+:stat-roll[Michizane rolls a 8 Damage]
+
+Bandit #1 is dead!
+
+:::stage-notes
+Bandits #2 and #3 are still trapped in the vines. Bandit #4 is unaffected by the vines, and Bandit #5 has fled the battle.
+:::
+
+Bandits #2 and #3 attempt to flee the battle.
+
+:stat-roll[Bandit #2 rolls a 13 Strength saving throw]
+
+:stat-roll[Bandit #3 rolls a 14 Strength saving throw]
+
+Both bandits remain trapped in the vines.
+
+Bandit #4 attacks Thoradin
+
+:stat-roll[Bandit #4 rolls a 3 Attack?]
+
+Thoradin lets out a loud laugh and attacks Bandit #4
+
+:stat-roll[Thoradin rolls a 3 + 2 + 15 Attack]
+
+Thoradin swings his two-handed axe at Bandit #4 and knocks him to the ground.
+
+:stat-roll[Thoradin rolls a 3 + 3 Damage]
+
+Bandit #4 is wounded but still alive
+
+Zaelrin attacks the wounded Bandit #4 with an arrow
+
+:stat-roll[Zaelrin rolls a 11 + 5 Attack]
+
+Zaelrin pierces Bandit #4 through the chest with his arrow
+
+:stat-roll[Zaelrin rolls a 4 + 2 (Stealth) Damage]
+
+Bandit #4 is dead!
+
+Michizane swings(?) at Bandit #2 with his quarter staff
+
+:stat-roll[Michizane rolls a 17 Attack]
+
+Bandit #2 was too slow to dodge the attack!
+
+:stat-roll[Michizane rolls a 2 + 3 Damage]
+
+Bandit #2 is wounded but still fighting.
+
+Bandit #2 Attacks Michizane in return
+
+:stat-roll[Bandit #2 rolls a 12 Attack]
+
+Bandit #2 whiffs his attack.
+
+Bandit #3 noticed Zaelrin's arrow kill Bandit #4 and attacks Zaelrin.
+
+:stat-roll[Bandit #3 rolls 18 Attack]
+
+Bandit #3 injures Zaelrin!
+
+:stat-roll[Bandit #3 rolls 2 Damage]
+
+Thoradin attacks Bandit #2 with his two-handed axe
+
+:stat-roll[Thoradin rolls 6 + 3 + 2 Attack]
+
+Bandit #2 narrowly dodges the attack
+
+Zaelrin attacks Bandit #3 with his rapier
+
+:stat-roll[Zaelrin rolls a 18 Attack]
+
+Zaelrin attacks before the bandit can dodge!
+
+:stat-roll[Zaelrin rolls a 3 + 3 Damage]
+
+Zaelrin manages to wound Bandit #3.
+
+Michizane attacks Bandit #2
+
+:stat-roll[Michizane rolls a 13 + 3 Attack]
+
+Michizane, still sore and frustrated from being knocked out of the tree earlier, unleashes a wave of angry attacks on the bandit.
+
+:stat-roll[Michizane rolls a 8 Damage]
+
+Bandit #2 is dead!
+
+Thoradin attacks Bandit #3
+
+:stat-roll[Thoradin rolls a 13 + 3 + 2 Attack]
+
+:stat-roll[Thoradin rolls a 7 + 3 Damage]
+
+Thoradin manages to kill the remaining bandit.
+
+Michizane off-handly shouts: "It's about time y'all do something!"
+
+The adventurers regroup and decide what to do next now that the battle has been won.
+
+Thoradin: "I guess we can work together"
+
+The party searches the bandits and nearby dropped loot.
+
+:stat-roll[Thoradin rolls a 17 + 0 Intelligence]
+
+No one finds any money or gold, but they each come across some glowing stones. They pick up the stones and immediately wake up back home in their own beds. Curiously, each adventurer still has a glowing stone...`;export{e as default};
