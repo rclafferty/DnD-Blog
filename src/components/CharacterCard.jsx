e059d156261c9ffd447843870922b802308968f8
Robert Lafferty <frogboythatsme@gmail.com>
2026-04-30 01:11:09 -0700

    Fixed character portait issues

--- src/components/CharacterCard.jsx
+++ src/components/CharacterCard.jsx
@@ -1,10 +1,12 @@
 export default function CharacterCard({ character }) {
+  console.log(JSON.stringify(character));
+  console.log(character.portrait);
+  
   return (
     <div className={`card text-center`}>
       <img
-        src={`${import.meta.env.BASE_URL}party/ElfDruid.jpg`}
+        src={`${import.meta.env.BASE_URL}${character.portrait}`}
         alt={character.name}
-        // className="w-32 h-32 object-cover rounded-full mx-auto mb-3 shadow-parchment"
       />
       <h2 className="text-xl font-bold">{character.name}</h2>
       <p className="meta italic mb-2">{character.class}</p>
